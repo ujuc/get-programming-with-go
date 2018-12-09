1ca94b6cae09a7cc20b3c86fb1aac9e0a724b053
Thomas Sungjin Kang <ujuc@ujuc.kr>
2018-12-09 11:47:22 +0900

    Changed rangeRandom
    * range_random > rangeRandom

--- lesson5/ticket-to-mars.go
+++ lesson5/ticket-to-mars.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func range_random(min, max int) int {
+func rangeRandom(min, max int) int {
 	return rand.Intn(max - min) + min
 }
 
@@ -32,16 +32,16 @@ func main() {
 		}
 
 		mars_away := 62100000
-		speed := range_random(16, 30)
+		speed := rangeRandom(16, 30)
 		days = mars_away / (speed * 60 * 60 * 24)
 
 		switch speed {
 		case 16, 17, 18, 19, 20:
-			price = range_random(36, 40)
+			price = rangeRandom(36, 40)
 		case 21, 22, 23, 24, 25:
-			price = range_random(40, 45)
+			price = rangeRandom(40, 45)
 		case 26, 27, 28, 29, 30:
-			price = range_random(46, 50)
+			price = rangeRandom(46, 50)
 		}
 
 		if num := rand.Intn(2); num == 0 {
