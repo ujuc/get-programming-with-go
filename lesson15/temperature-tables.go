d9911394e5d189d1f6ad138570ba9e52e3af8278
Thomas Sungjin Kang <ujuc@ujuc.kr>
2019-01-03 22:55:27 +0900

    lesson 15 - 조건에 맞게 수정
    * 1급함수로 나머지 부분 채워넣음.

--- lesson15/temperature-tables.go
+++ lesson15/temperature-tables.go
@@ -5,48 +5,49 @@ import (
 	"strings"
 )
 
-type celsius float64
-type fahrenheit float64
 type temperature string
 
-func drawTable(t temperature) {
-	switch t {
-	case "c":
-		fmt.Println(strings.Repeat("=", 23))
-		fmt.Printf("| %-8v | %-8v |\n", "'C", "'F")
-		fmt.Println(strings.Repeat("=", 23))
-
-		var step celsius
-		for step = -40.0; step <= 100.0; step += 5 {
-			var c = step
-			fmt.Printf("| %-8v | %-8v |\n", c, c.fahrenheit())
-		}
-		fmt.Println(strings.Repeat("=", 23))
-		break
-	case "f":
-		fmt.Println(strings.Repeat("=", 23))
-		fmt.Printf("| %-8v | %-8v |\n", "'F", "'C")
-		fmt.Println(strings.Repeat("=", 23))
-
-		var step fahrenheit
-		for step = -40.0; step <= 100.0; step += 5 {
-			var f = step
-			fmt.Printf("| %-8v | %-8.2f |\n", f, f.celsius())
-		}
-		fmt.Println(strings.Repeat("=", 23))
-	}
+var drawTable = func(t1 float64, t2 float64) {
+	fmt.Printf("| %8.2f | %8.2f |\n", t1, t2)
 }
-
-func (c celsius) fahrenheit() fahrenheit {
-	return fahrenheit((c * 9.0 / 5.0) + 32.0)
-}
-
-func (f fahrenheit) celsius() celsius {
-	return celsius((f - 32.0) * 5.0 / 9.0)
-}
-
 
 func main() {
-	drawTable("c")
-	drawTable("f")
+	title := func (t temperature) {
+		switch t {
+		case "c":
+			fmt.Println(strings.Repeat("=", 23))
+			fmt.Printf("| %8v | %8v |\n", "'C", "'F")
+			fmt.Println(strings.Repeat("=", 23))
+			break
+		case "f":
+			fmt.Println(strings.Repeat("=", 23))
+			fmt.Printf("| %8v | %8v |\n", "'F", "'C")
+			fmt.Println(strings.Repeat("=", 23))
+			break
+		}
+	}
+
+	bottom := func() {
+		fmt.Println(strings.Repeat("=", 23))
+	}
+
+	f := func(c float64) float64 {
+		return (c * 9.0 / 5.0) + 32.0
+	}
+
+	c := func(f float64) float64 {
+		return (f - 32.0) * 5.0 / 9.0
+	}
+
+	title("c")
+	for step := -40.0; step <= 100.0; step += 5 {
+		drawTable(step, f(step))
+	}
+	bottom()
+
+	title("f")
+	for step := -40.0; step <= 100.0; step += 5 {
+		drawTable(step, c(step))
+	}
+	bottom()
 }
