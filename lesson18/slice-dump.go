7aa077fa3826c8a598250b4399d6b94d87bfd18c
Thomas SJ Kang <ujuc@ujuc.kr>
2019-06-02 22:26:57 +0900

    Lesson18

---
+++ lesson18/slice-dump.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func dump(label string, slice []string) {
+	fmt.Printf("%v: length %v, capacity %v %v\n", label, len(slice), cap(slice), slice)
+}
+
+func main() {
+	dwarfs := []string{"Ceres", "Pluto", "Haumea", "Makemake", "Eris"}
+	dump("dwarfs", dwarfs)
+	dump("dwarfs[1:2]", dwarfs[1:2])
+}
